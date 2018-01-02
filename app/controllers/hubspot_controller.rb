04b3d39472e5ef1d02dc2b1fecee567aac02c18a
ch.walker@gmail.com <ch.walker@gmail.com>
2018-01-02 10:48:09 -0500

    Updated test involvements

--- app/controllers/hubspot_controller.rb
+++ app/controllers/hubspot_controller.rb
@@ -3,60 +3,8 @@ class HubspotController < ApplicationController
   def involvements
     json = {
         "results" => [
-        {
-            "objectId" => 567,
-            "title"=> "Involvements",
-            "link"=> "http=>//saponline.htri.net",
-            "created"=> "2016-09-15",
-            "priority"=> "HIGH",
-            "project"=> "API",
-            "RelationshipType" => "HTRI",
-            "InvolvementType" => "Member",
-            "reported_by"=> "msmith@hubspot.com",
-            "description"=> "Customer reported that the APIs are just running too fast. This is causing a problem in that they're so happy.",
-            "reporter_type"=> "Account Manager",
-            "status"=> "In Progress",
-            "ticket_type"=> "Bug",
-            "updated"=> "2016-09-28",
-            "actions"=> [{
-                "type"=> "IFRAME",
-                "width"=> 890,
-                "height"=> 748,
-                "uri"=> "https=>//example.com/edit-iframe-contents",
-                "label"=> "Edit",
-                "associatedObjectProperties"=> []
-              },
-              {
-                "type"=> "IFRAME",
-                "width"=> 890,
-                "height"=> 748,
-                "uri"=> "https=>//example.com/reassign-iframe-contents",
-                "label"=> "Reassign",
-                "associatedObjectProperties"=> []
-              },
-              {
-                "type"=> "ACTION_HOOK",
-                "httpMethod"=> "PUT",
-                "associatedObjectProperties"=> [],
-                "uri"=> "https=>//example.com/tickets/245/resolve",
-                "label"=> "Resolve"
-              },
-              {
-                "type"=> "CONFIRMATION_ACTION_HOOK",
-                "confirmationMessage"=> "Are you sure you want to delete this ticket?",
-                "confirmButtonText"=> "Yes",
-                "cancelButtonText"=> "No",
-                "httpMethod"=> "DELETE",
-                "associatedObjectProperties"=> [
-                  "protected_account"
-                ],
-                "uri"=> "https=>//example.com/tickets/245",
-                "label"=> "Delete"
-            }
-            ],
-            },
-            {
-                "objectId"=> 567,
+          {
+            "objectId"=> 573,
             "title"=> "Another Involvement",
             "link"=> "http=>//example.com/2",
             "created"=> "2016-08-04",
@@ -71,54 +19,28 @@ class HubspotController < ApplicationController
             "ticket_type"=> "Bug",
             "updated"=> "2016-09-23",
             "properties"=> [
-            {
-                "label"=> "Resolved by",
-            "dataType"=> "EMAIL",
-            "value"=> "ijones@hubspot.com"
-        },
-            {
-                "label"=> "Resolution type",
-            "dataType"=> "STRING",
-            "value"=> "Referred to documentation"
-        },
-            {
-                "label"=> "Resolution impact",
-            "dataType"=> "CURRENCY",
-            "value"=> "94.34",
-            "currencyCode"=> "GBP"
-        }
-        ],
-            "actions"=> [
-            {
-                "type"=> "IFRAME",
-            "width"=> 890,
-            "height"=> 748,
-            "uri"=> "https=>//example.com/edit-iframe-contents",
-            "label"=> "Edit"
-        },
-            {
-                "type"=> "CONFIRMATION_ACTION_HOOK",
-            "confirmationMessage"=> "Are you sure you want to delete this ticket?",
-            "confirmButtonText"=> "Yes",
-            "cancelButtonText"=> "No",
-            "httpMethod"=> "DELETE",
-            "associatedObjectProperties"=> [
-            "protected_account"
-        ],
-            "uri"=> "https=>//example.com/tickets/245",
-            "label"=> "Delete"
-        }
-        ]
-        }
-        ],
-            "settingsAction"=> {
+              {
+                "label"=> "Involvement Code",
+                "dataType"=> "STRING",
+                "value"=> "00000001"
+              },
+              {
+                "label"=> "Relationship Type",
+                "dataType"=> "STRING",
+                "value"=> "HTRI"
+              },
+            ],
+            "actions"=> []
+          }],
+
+        "settingsAction"=> {
             "type"=> "IFRAME",
             "width"=> 890,
             "height"=> 748,
             "uri"=> "https=>//example.com/settings-iframe-contents",
             "label"=> "Settings"
         },
-            "primaryAction"=> {
+        "primaryAction"=> {
             "type"=> "IFRAME",
             "width"=> 890,
             "height"=> 748,
